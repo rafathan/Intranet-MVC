bd06ee63a7f8e9a00e380e0d02b8d8df02ba0c7a
michaeldevenney <mdevenney@entech.com>
2013-10-22 23:34:17 -0400

    Bootstrap

---
+++ Intranet-MVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intranet_MVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        //
+        // GET: /Departments/
+
+        public ActionResult IT()
+        {
+            return View();
+        }
+
+        public ActionResult HR()
+        {
+            return View();
+        }
+
+        public ActionResult PM()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
